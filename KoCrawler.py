f1fa7dbe303f07c30aad5ec25f68ca54136207df
김보석 <boseoggim08@gmail.com>
2022-05-02 20:14:59 +0900

    한국어 크롤링 코드 작성 시작

--- KoCrawler.py
+++ KoCrawler.py
@@ -1,21 +1,29 @@
-# goodreads라는 영어 명언 사이트에서 크롤링
+# saramro 한국어 명언 사이트에서 크롤링
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from pandas import DataFrame
 
-data = pd.read_csv('./dataset.csv')
+#data = pd.read_csv('./dataset.csv')
 
 
 TAG = ['love', 'life', 'inspirational', 'humor', 'philosophy', 'god', 'truth', 'wisdom', 'poetry', 'romance', 'death', 'happiness', 'hope', 'faith', 'quotes', 'life-lessons', 'writing', 'motivational', 'religion', 'success', 'time', 'knowledge', 'science']
 t = 'science'
 
+url = 'https://saramro.com/quotes'
+
+response = requests.get(url)
+print(response.text)
+
+
+'''
 for page in range(1, 101):
     url = 'https://www.goodreads.com/quotes/tag/' + t + '?page=' + str(page)
 
     response = requests.get(url)
     print(page)
+    
     if response.status_code == 200:
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -31,3 +39,4 @@ for page in range(1, 101):
 
 
 data.to_csv('dataset.csv', index=False)
+'''
